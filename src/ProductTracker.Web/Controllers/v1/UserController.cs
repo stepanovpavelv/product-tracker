171b5b0ada5c00e93156492d4e2984895a5c7d94
Stepanov.PV <Stepanov.PV@parma.ru>
2024-06-17 11:31:14 +0400

    added todo and old controller deleted (2)

--- src/ProductTracker.Web/Controllers/v1/UserController.cs
+++ src/ProductTracker.Web/Controllers/v1/UserController.cs
@@ -8,58 +8,60 @@ using ProductTracker.Application.Users.Response;
 using ProductTracker.Web.Extensions;
 using ProductTracker.Web.Model;
 
-namespace ProductTracker.Web.Controllers.v1
+namespace ProductTracker.Web.Controllers.v1;
+
+// TODO: make urls as oauth-urls
+// https://auth0.com/docs/secure/tokens/refresh-tokens/get-refresh-tokens
+
+[ApiController]
+[ApiVersion( 1.0 )]
+[Route("api/v{version:apiVersion}/[controller]")]
+public class UserController(IMediator mediator) : ControllerBase
 {
-    [ApiController]
-    [ApiVersion( 1.0 )]
-    [Route("api/v{version:apiVersion}/[controller]")]
-    public class UserController(IMediator mediator) : ControllerBase
-    {
-        private readonly IMediator _mediator = mediator;
+    private readonly IMediator _mediator = mediator;
 
-        /// <summary>
-        /// Регистрация нового пользователя.
-        /// </summary>
-        /// <response code="200">Возвращает идентификатор зарегистрированного пользователя.</response>
-        /// <response code="400">Возвращает перечень валидационных ошибок при некорректном запросе.</response>
-        /// <response code="500">Произошла непредвиденная ошибка сервиса.</response>
-        [HttpPost]
-        [Consumes(MediaTypeNames.Application.Json)]
-        [Produces(MediaTypeNames.Application.Json)]
-        [ProducesResponseType(typeof(ApiResponse<RegisteredUserResponse>), StatusCodes.Status200OK)]
-        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status500InternalServerError)]
-        public async Task<IActionResult> Create([FromBody][Required] RegisterUserCommand command) =>
-            (await _mediator.Send(command)).ToActionResult();
+    /// <summary>
+    /// Регистрация нового пользователя.
+    /// </summary>
+    /// <response code="200">Возвращает идентификатор зарегистрированного пользователя.</response>
+    /// <response code="400">Возвращает перечень валидационных ошибок при некорректном запросе.</response>
+    /// <response code="500">Произошла непредвиденная ошибка сервиса.</response>
+    [HttpPost]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [Produces(MediaTypeNames.Application.Json)]
+    [ProducesResponseType(typeof(ApiResponse<RegisteredUserResponse>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> Create([FromBody][Required] RegisterUserCommand command) =>
+        (await _mediator.Send(command)).ToActionResult();
 
-        /// <summary>
-        /// Аутентификация пользователя.
-        /// </summary>
-        /// <response code="200">Возвращает аутентификационные данные.</response>
-        /// <response code="400">Возвращает перечень валидационных ошибок при некорректном запросе.</response>
-        /// <response code="500">Произошла непредвиденная ошибка сервиса.</response>
-        [HttpPost]
-        [Consumes(MediaTypeNames.Application.Json)]
-        [Produces(MediaTypeNames.Application.Json)]
-        [ProducesResponseType(typeof(ApiResponse<AuthTokenUserResponse>), StatusCodes.Status200OK)]
-        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status500InternalServerError)]
-        public async Task<IActionResult> Login([FromBody][Required] LoginUserCommand command) =>
-            (await _mediator.Send(command)).ToActionResult();
+    /// <summary>
+    /// Аутентификация пользователя.
+    /// </summary>
+    /// <response code="200">Возвращает аутентификационные данные.</response>
+    /// <response code="400">Возвращает перечень валидационных ошибок при некорректном запросе.</response>
+    /// <response code="500">Произошла непредвиденная ошибка сервиса.</response>
+    [HttpPost]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [Produces(MediaTypeNames.Application.Json)]
+    [ProducesResponseType(typeof(ApiResponse<AuthTokenUserResponse>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> Login([FromBody][Required] LoginUserCommand command) =>
+        (await _mediator.Send(command)).ToActionResult();
 
-        /// <summary>
-        /// Обновление токенов доступа пользователя к системе.
-        /// </summary>
-        /// <response code="200">Возвращает аутентификационные данные.</response>
-        /// <response code="400">Возвращает перечень валидационных ошибок при некорректном запросе.</response>
-        /// <response code="500">Произошла непредвиденная ошибка сервиса.</response>
-        [HttpPost]
-        [Consumes(MediaTypeNames.Application.Json)]
-        [Produces(MediaTypeNames.Application.Json)]
-        [ProducesResponseType(typeof(ApiResponse<AuthTokenUserResponse>), StatusCodes.Status200OK)]
-        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status500InternalServerError)]
-        public async Task<IActionResult> UpdateCredentials([FromBody][Required] UpdateUserRefreshTokenCommand command) =>
-            (await _mediator.Send(command)).ToActionResult();
-    }
+    /// <summary>
+    /// Обновление токенов доступа пользователя к системе.
+    /// </summary>
+    /// <response code="200">Возвращает аутентификационные данные.</response>
+    /// <response code="400">Возвращает перечень валидационных ошибок при некорректном запросе.</response>
+    /// <response code="500">Произошла непредвиденная ошибка сервиса.</response>
+    [HttpPost]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [Produces(MediaTypeNames.Application.Json)]
+    [ProducesResponseType(typeof(ApiResponse<AuthTokenUserResponse>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> UpdateCredentials([FromBody][Required] UpdateUserRefreshTokenCommand command) =>
+        (await _mediator.Send(command)).ToActionResult();
 }

1f08a089a95578bc8f77f277dbea2caa2b81c910
stepanov.pavel.v <Stepanov.PV@parma.ru>
2024-07-05 00:14:40 +0400

    added goods controller method

--- src/ProductTracker.Web/Controllers/v1/GoodsController.cs
+++ src/ProductTracker.Web/Controllers/v1/GoodsController.cs
@@ -1,6 +1,12 @@
-﻿using Asp.Versioning;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net.Mime;
+using Asp.Versioning;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using ProductTracker.Application.Users.Command;
+using ProductTracker.Application.Users.Response;
+using ProductTracker.Web.Model;
+using ProductTracker.Web.Extensions;
 
 namespace ProductTracker.Web.Controllers.v1
 {
@@ -10,5 +16,25 @@ namespace ProductTracker.Web.Controllers.v1
     public class GoodsController(IMediator mediator) : ControllerBase
     {
         private readonly IMediator _mediator = mediator;
+
+        /// <summary>
+        /// Добавление новой карточки товара в систему.
+        /// </summary>
+        /// <response code="200">Возвращает идентификатор зарегистрированного товара.</response>
+        /// <response code="400">Возвращает перечень валидационных ошибок при некорректном запросе.</response>
+        /// <response code="409">Товар с таким наименованием уже был ранее создан.</response>
+        /// <response code="500">Произошла непредвиденная ошибка сервиса.</response>
+        [HttpPost]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(typeof(ApiResponse<RegisteredUserResponse>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status409Conflict)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Create([FromBody][Required] RegisterUserCommand command) =>
+            (await _mediator.Send(command)).ToActionResult();
+
+        // todo: добавить индекс по наименованию товара
+        // todo: добавить колонки с временем создания карточки и юзера, который создал
     }
 }
